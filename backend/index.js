986e92ba1e01ba3025a9d9d8a3f4bafbd9e9e01a
Deepak Rai <deepakmegtech@gmail.com>
2024-08-29 08:38:45 +0530

    Database connections

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,26 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import mongoose from 'mongoose'
 
 const app = express()
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000')
+dotenv.config()
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+
+const PORT = process.env.PORT || 4002
+const URI = process.env.MongoDBURI
+
+try {
+  mongoose.connect(URI)
+  console.log('Database connected')
+} catch (error) {
+  console.log('Error connecting Database')
+}
+
+app.listen(PORT, () => {
+  console.log('Server listening on port', PORT)
 })
