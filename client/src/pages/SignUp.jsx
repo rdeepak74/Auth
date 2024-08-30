827530a3d18be0443f2c99d3cc8d74a0f15f6c83
Deepak Rai <deepakmegtech@gmail.com>
2024-08-30 08:36:05 +0530

    signup ui

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -28,7 +28,7 @@ export default function SignUp() {
         </button>
       </form>
       <div className="flex gap-2 mt-5 mx-4">
-        <p>Have an account</p>
+        <p>Have an account?</p>
         <Link to="/sign-in">
           <span className="text-blue-500">Sign in</span>
         </Link>
