2dbdf9b3a38ed360e29eb053ebc2080b789cdf23
Deepak Rai <deepakmegtech@gmail.com>
2024-08-31 18:42:47 +0530

    Sign up functionality done

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,13 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import cors from 'cors'
+// import cors from 'cors'
 import mongoose from 'mongoose'
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js'
 const app = express()
 
 dotenv.config()
-app.use(cors())
+// app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,30 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 export default function SignUp() {
+  const [formData, setFormData] = useState({})
+  const [error, setError] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const handleChange = (e) => {
+    setFormData((prevData) => ({ ...prevData, [e.target.id]: e.target.value }))
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      // const response = await fetch(
+      //   'http://localhost:4001/api/auth/signup',
+      //   formData
+      // )
+      setLoading(true)
+      const response = await axios.post('api/auth/signup', formData)
+      const data = response.data
+      console.log(data)
+      setLoading(false)
+      setError(false)
+    } catch (error) {
+      setError(true)
+      setLoading(false)
+      // console.error(error)
+    }
+  }
   return (
     <div className="max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-5">Sign Up</h1>
-      <form className="flex flex-col gap-4 mx-4">
+      <form className="flex flex-col gap-4 mx-4" onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="username"
           id="username"
+          required
           className="bg-slate-100 outline-none rounded-lg p-3"
+          onChange={handleChange}
         />
         <input
           type="email"
           placeholder="email"
           id="email"
+          required
           className="bg-slate-100 outline-none rounded-lg p-3"
+          onChange={handleChange}
         />
         <input
           type="password"
           placeholder="password"
           id="password"
+          required
           className="bg-slate-100 outline-none rounded-lg p-3"
+          onChange={handleChange}
         />
-        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-          Sign Up
+        <button
+          disabled={loading}
+          className="bg-slate-700  text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? 'Loading...' : 'Sign Up'}
         </button>
       </form>
       <div className="flex gap-2 mt-5 mx-4">
@@ -33,6 +69,7 @@ export default function SignUp() {
           <span className="text-blue-500">Sign in</span>
         </Link>
       </div>
+      <p className="text-red-700 mt-5 mx-4">{error && 'Somthing went wrong'}</p>
     </div>
   )
 }
